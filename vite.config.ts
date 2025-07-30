d6c3a43cf61ba81622124271c0d19d6a0a8d28ce
Olivos Sistem <olivos@sistem.com>
2025-07-30 10:14:36 +0300

    fix: Vite config rollup external ayarları temizlendi

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['react-is'],
+      external: [],
       output: {
         manualChunks: undefined,
       },
